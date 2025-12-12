f6f94db36132a3e69ea7c9b8f9c0a5520f90d24b
Martin Joshua Garaza <57517246+stronze21@users.noreply.github.com>
2025-12-12 14:41:47 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,6 @@ use App\Http\Controllers\Api\QueueController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-
-
-
-
-
-
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
